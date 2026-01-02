3a5facd6972f4293d223d05e3a929a88383aa89c
Sai <sarany1947@gmail.com>
2026-01-02 11:00:24 -0600

    Day 1: Implementation of Identity Foundation

--- infrastructure/environments/prod/variables.tf
+++ infrastructure/environments/prod/variables.tf
@@ -2,4 +2,7 @@ variable "project_id" {
   type        = string
   description = "The GCP Project ID"
 }
-variable "github_repo" { type = string } # Matches the module's input
+variable "github_repo" {
+  description = "The GitHub repository to trust for WIF"
+  type        = string
+} # Matches the module's input
